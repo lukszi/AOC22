5fa7fc2dc8215bb79da8198c753dc5d49a6253ab
Lukas Szimtenings <lszimtenings@ukaachen.de>
2022-12-12 21:40:34 +0100

    Refactor visibility check

--- 8 Treetop Tree House/part 1.py
+++ 8 Treetop Tree House/part 1.py
@@ -10,30 +10,27 @@ def count_visible_trees(height_map) -> int:
     visible_trees = len(height_map) * 2 + len(height_map[0]) * 2 - 4
     for i in range(1, len(height_map) - 1):
         for j in range(1, len(height_map[0]) - 1):
-            visible_left = True
-            visible_right = True
-            visible_top = True
-            visible_bottom = True
+            visibility = [True] * 4
             for k in range(0, i):
                 if height_map[k][j] >= height_map[i][j]:
-                    visible_top = False
+                    visibility[0] = False
                     break
-            if not visible_top:
+            if not visibility[0]:
                 for k in range(i + 1, len(height_map)):
                     if height_map[k][j] >= height_map[i][j]:
-                        visible_bottom = False
+                        visibility[1] = False
                         break
-            if not visible_top and not visible_bottom:
+            if not visibility[0] and not visibility[1]:
                 for k in range(0, j):
                     if height_map[i][k] >= height_map[i][j]:
-                        visible_left = False
+                        visibility[2] = False
                         break
-            if not visible_top and not visible_bottom and not visible_left:
+            if not visibility[0] and not visibility[1] and not visibility[2]:
                 for k in range(j + 1, len(height_map[i])):
                     if height_map[i][k] >= height_map[i][j]:
-                        visible_right = False
+                        visibility[3] = False
                         break
-            if visible_top or visible_bottom or visible_left or visible_right:
+            if visibility[0] or visibility[1] or visibility[2] or visibility[3]:
                 visible_trees += 1
 
     return visible_trees
